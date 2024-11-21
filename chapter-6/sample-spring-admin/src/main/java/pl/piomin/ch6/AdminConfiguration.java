93afc3a9fba965356749a756c6ae1f9d9b397d2b
piomin <piotr.minkowski@gmail.com>
2024-11-22 00:23:46 +0100

    adding spring boot admin

---
+++ chapter-6/sample-spring-admin/src/main/java/pl/piomin/ch6/AdminConfiguration.java
@@ -0,0 +1,17 @@
+package pl.piomin.ch6;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AdminConfiguration {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
+                .csrf().disable()
+                .build();
+    }
+}
