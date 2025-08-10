f9f9d8051b60bed44251768f3b2f4ded00389e1f
piomin <piotr.minkowski@gmail.com>
2025-08-10 13:29:31 +0200

    Add spring kafka app to chapter-7

---
+++ chapter-7/sample-spring-kafka/src/main/java/pl/piomin/services/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,44 @@
+package pl.piomin.services.kafka.consumer;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@SpringBootApplication
+@EnableKafka
+public class KafkaConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumer.class, args);
+    }
+
+    @Value("${app.in.topic}")
+    private String topic;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> listenerFactory(ConsumerFactory<String, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+    @Bean
+    public NewTopic infoTopic() {
+        return TopicBuilder.name(topic)
+                .partitions(10)
+                .replicas(3)
+                .build();
+    }
+
+}
---
+++ chapter-7/sample-spring-kafka/src/main/java/pl/piomin/services/kafka/consumer/message/Info.java
@@ -0,0 +1,20 @@
+package pl.piomin.services.kafka.consumer.message;
+
+public class Info {
+
+    private long id;
+    private String message;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
