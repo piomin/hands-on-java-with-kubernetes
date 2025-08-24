ce1a13e2f388e4cf7732d92bb5cc61f6269dedb1
piomin <piotr.minkowski@gmail.com>
2025-08-24 23:45:41 +0200

    fix app chapter-9

--- chapter-9/sample-spring-k8s-ch9/src/test/java/pl/piomin/ch9/PersonControllerUnitTests.java
+++ chapter-9/sample-spring-k8s-ch9/src/test/java/pl/piomin/ch9/PersonControllerUnitTests.java
@@ -1,7 +1,6 @@
 package pl.piomin.ch9;
 
 import org.instancio.Instancio;
-import org.instancio.Select;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +12,13 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import pl.piomin.ch9.controller.PersonController;
 import pl.piomin.ch9.domain.Person;
 import pl.piomin.ch9.repository.PersonRepository;
-import pl.piomin.ch9.repository.PersonViewRepository;
-import pl.piomin.ch9.view.PersonView;
+
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PersonController.class)
@@ -30,8 +30,6 @@ public class PersonControllerUnitTests {
     MockMvc mvc;
     @MockBean
     PersonRepository repository;
-    @MockBean
-    PersonViewRepository viewRepository;
 
     @Test
     public void testAdd() throws Exception {
@@ -46,12 +44,12 @@ public class PersonControllerUnitTests {
 
     @Test
     public void testFind() throws Exception {
-        PersonView person = Instancio.of(PersonView.class).create();
-        when(viewRepository.findOne(person.getId())).thenReturn(person);
+        Person person = Instancio.of(Person.class).create();
+        when(repository.findById(person.getId())).thenReturn(Optional.of(person));
         mvc.perform(get("/{}", person.getId()).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.name", is(person.getName())));
+                .andExpect(jsonPath("$.firstName", is(person.getFirstName())));
     }
 
 
