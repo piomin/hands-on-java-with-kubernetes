7ebb4d82d9feab8f05196f3cb2c48b9542840145
piomin <piotr.minkowski@gmail.com>
2025-11-14 10:47:16 +0100

    add sample-quarkus-on-kubernetes

---
+++ chapter-3/sample-quarkus-on-kubernetes/src/main/java/pl/piomin/resource/PersonResource.java
@@ -0,0 +1,50 @@
+package pl.piomin.resource;
+
+import pl.piomin.domain.Person;
+import pl.piomin.repository.PersonRepository;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import java.util.List;
+
+@Path("/persons")
+public class PersonResource {
+
+    @Inject
+    PersonRepository personRepository;
+
+    @POST
+    @Transactional
+    public Person addPerson(Person person) {
+        personRepository.persist(person);
+        return person;
+    }
+
+    @GET
+    public List<Person> getPersons() {
+        return personRepository.listAll();
+    }
+
+    @GET
+    @Path("/name/{name}")
+    public List<Person> getPersonsByName(@PathParam("name") String name) {
+        return personRepository.findByName(name);
+    }
+
+    @GET
+    @Path("/age-greater-than/{age}")
+    public List<Person> getPersonsByAge(@PathParam("age") int age) {
+        return personRepository.findByAgeGreaterThan(age);
+    }
+
+    @GET
+    @Path("/{id}")
+    public Person getPersonById(@PathParam("id") Long id) {
+        return personRepository.findById(id);
+    }
+
+}
